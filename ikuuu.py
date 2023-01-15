c8745d2564551f66d3069c95960e1148028fcd1b
剁椒鱼头 <1489221272@qq.com>
2023-01-15 20:19:47 +0800

    update

--- ikuuu.py
+++ ikuuu.py
@@ -4,12 +4,12 @@ import requests, json,re,os,sys
 from datetime import datetime
 from sendNotify import send
 #检测账户变量
-#ikuuu = os.environ.get("ikuuu") if os.environ.get("ikuuu") else ""
-#if not ikuuu:
-#    print("⚠️未发现有效cookie,退出程序!")
-#    sys.exit()
+ikuuu = os.environ.get("ikuuu") if os.environ.get("ikuuu") else ""
+if not ikuuu:
+    print("⚠️未发现有效cookie,退出程序!")
+    sys.exit()
 #分割账户
-cookie = [lang=zh-cn; uid=516722; email=1489221272@qq.com; key=80b92271513a6ddc5a5fabb61e00e5ebc8df8780397dd; ip=97ed8a28bc393bbf6066efb295aab96f; expire_in=1674389105]
+cookie = ikuuu
 #print(cookie)
 
 url_info = 'https://ikuuu.dev/user/profile'
@@ -24,6 +24,6 @@ result = json.loads(html.text)['msg']
 info = "".join(re.findall('<div class="d-sm-none d-lg-inline-block">(.*?)</div>', html_info, re.S))
 print(info+'\n'+result)
 # 执行完毕发送通知
-#title = '🔁ikuuu机场-签到'
-#msg = f"⏰{str(datetime.now())[:19]}\n" + '✅签到成功'
-#send(title,msg)
+title = '🔁ikuuu机场-签到'
+msgtext = f"⏰{str(datetime.now())[:19]}\n" + '✅签到成功'
+send(title,msgtext)
