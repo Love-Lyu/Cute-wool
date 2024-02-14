98e959d0a6ff359ca6cbde780b8d9ad867634cfa
Loveyu <59733455+Love-Lyu@users.noreply.github.com>
2024-02-15 00:03:25 +0800

    update

--- xkdl.py
+++ xkdl.py
@@ -19,15 +19,11 @@ try:
     xingkong = os.environ["xingkong"]
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-        'Connection': 'keep-alive',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # Requests sorts cookies= alphabetically
-        # 'Cookie': 'ASP.NET_SessionId=23dfn2mafqhzkuuzosirclt1; Hm_lvt_d76458121a7604d3e55d998f66ef0be6=1659492634; dt_cookie_user_name_remember=DTcms=18729469208; Hm_lpvt_d76458121a7604d3e55d998f66ef0be6=1659493214',
-        'DNT': '1',
         'Origin': 'http://www.xkdaili.com',
-        'Referer': 'http://www.xkdaili.com/main/usercenter.aspx',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77',
+        'Referer': 'https://www.xkdaili.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
         'X-Requested-With': 'XMLHttpRequest',
     }
     # 用于拼接
